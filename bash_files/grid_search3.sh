4165b5b44ed309eee1f30b6608e9ea379b5fd7e9
olive <olivedunphy99@gmail.com>
2025-02-17 13:18:03 +0000

    grid search 3

--- bash_files/grid_search3.sh
+++ bash_files/grid_search3.sh
@@ -2,7 +2,7 @@
 #SBATCH --partition=gpu
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --gres=gpu:gtx_1080_ti:3
+#SBATCH --gres=gpu:3        # Request 3 GPUs without specifying type
 #SBATCH --mem=32000M
 
 export NUMEXPR_MAX_THREADS=8
